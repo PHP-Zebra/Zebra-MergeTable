7b5a7a0382873e3adc7069dfcf0b15a2a6ac5919
huyanping <hypxm@qq.com>
2014-11-24 22:45:53 +0800

    add MergeTable class

---
+++ src/Zebra/MergeTable/Exception/DBCallbackIllegalMergeTableException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 14-11-24
+ * Time: 下午10:31
+ *
+ * 数据库回调函数不可用异常
+ */
+
+namespace Zebra\MergeTable\Exception;
+
+
+class DBCallbackIllegalMergeTableException extends \Exception{
+
+} 
--- src/Zebra/MergeTable/MergeTable.php
+++ src/Zebra/MergeTable/MergeTable.php
@@ -10,6 +10,52 @@
 
 namespace Zebra\MergeTable;
 
+use Zebra\MergeTable\Exception\DBCallbackIllegalMergeTableException;
+
 class MergeTable {
 
+    /**
+     * @var callable 数据库驱动回调函数
+     */
+    protected $database_callback;
+
+    /**
+     * @var 合并表数组
+     */
+    protected $tables;
+
+    /**
+     * @param $database_callback 数据库驱动回调函数
+     * @throws Exception\DBCallbackIllegalMergeTableException
+     */
+    public function __construct($database_callback=null){
+        if(is_null($database_callback)) return ;
+
+        if(!is_callable($database_callback)){
+            throw new DBCallbackIllegalMergeTableException;
+        }
+        $this->database_callback = $database_callback;
+    }
+
+    /**
+     * @param $table_name 添加合并表
+     */
+    public function addTable($table_name){
+        $this->tables[] = $table_name;
+    }
+
+    /**
+     * @param $query 执行查询，返回结果
+     */
+    public function fetchAll($query){
+        //SQL语句分析、拆分
+
+        //获取查询结果
+
+        //合并结果
+
+        //结果运算(SUM、COUNT等)
+
+        //返回结果
+    }
 } 
