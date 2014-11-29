b6777e77487a3234d0f5d17b07a667461c52193d
huyanping <hypxm@qq.com>
2014-11-29 16:36:07 +0800

    update

--- src/Zebra/MergeTable/MergeTable.php
+++ src/Zebra/MergeTable/MergeTable.php
@@ -13,14 +13,15 @@ namespace Zebra\MergeTable;
 use Zebra\MergeTable\Exception\DBCallbackIllegalMergeTableException;
 use Zebra\MergeTable\Exception\SQLExecuteErrorException;
 
-class MergeTable {
+class MergeTable
+{
 
     /**
      * @var callable 数据库驱动回调函数
      */
     protected $database_callback;
 
-    protected $error_callback;
+    protected $sql_error_handle;
 
     /**
      * @var 合并表数组
@@ -29,59 +30,69 @@ class MergeTable {
 
     /**
      * @param $database_callback 数据库驱动回调函数
+     * @param null $error_callback
      * @throws Exception\DBCallbackIllegalMergeTableException
+     * @throws Exception\SQLExecuteErrorException
      */
-    public function __construct($database_callback=null, $error_callback=null){
-        if(is_null($database_callback) && is_null($error_callback)) return ;
+    public function __construct($database_callback = null, $sql_error_handle = null)
+    {
+        if (is_null($database_callback) && is_null($sql_error_handle)) return;
 
-        if(!is_callable($database_callback)){
+        if (!is_callable($database_callback)) {
             throw new DBCallbackIllegalMergeTableException;
         }
         $this->database_callback = $database_callback;
 
-        if(!is_callable($error_callback)){
+        if (!is_callable($sql_error_handle)) {
             throw new SQLExecuteErrorException;
         }
-        $this->error_callback = $error_callback;
+        $this->sql_error_handle = $sql_error_handle;
     }
 
     /**
      * @param $table_name 添加合并表
      */
-    public function addTable($table_name){
+    public function addTable($table_name)
+    {
         $this->tables[] = $table_name;
     }
 
     /**
      * @param $query 执行查询，返回结果
      */
-    public function fetchAll($query){
+    public function fetchAll($query)
+    {
         //获取查询结果
+        $parser = new \PHPSQL\Parser($query);
+        $parsed_query = $parser->parsed;
+
+        $result = $this->getAllTableResult($parsed_query);
         //合并结果
-
-        //结果运算(SUM、COUNT等)
-
+        $result_merge_model = new ResultMerge();
+        $merge_result = $result_merge_model->merge($result, $parsed_query);
         //返回结果
+        return $merge_result;
     }
 
-    protected function getAllTableResult($parsed_query){
+    protected function getAllTableResult($parsed_query)
+    {
         $creator = new \PHPSQL\Creator();
-        try{
-            foreach($this->tables as $table){
+        try {
+            foreach ($this->tables as $table) {
                 $parsed = $parsed_query;
                 $parsed['FROM'][0]['table'] = $table;
                 $query = $creator->create($parsed);
                 $temp_result = $this->database_callback($query);
-                if($temp_result === false){
+                if ($temp_result === false) {
                     return false;
                 }
 
                 $result[$table] = $temp_result;
             }
-        }catch (\Exception $e){
-            if(!empty($this->error_callback)){
-                $this->error_callback($query);
-            }else{
+        } catch (\Exception $e) {
+            if (!empty($this->error_callback)) {
+                $this->sql_error_handle($query);
+            } else {
                 throw $e;
             }
         }
